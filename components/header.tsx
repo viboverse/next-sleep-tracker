280f904b4c082a5ff10263847ae11fcee8d423e0
vibo <vahab.afsharian94@gmail.com>
2025-09-28 23:05:56 +0300

    fix: Adjust padding in Header component for consistent layout

--- components/header.tsx
+++ components/header.tsx
@@ -9,7 +9,7 @@ export default async function Header() {
 
   return (
     <nav>
-      <div className="mx-auto max-w-7xl border-b-2 border-gray-200 sm:px-6 lg:px-8">
+      <div className="mx-auto max-w-7xl border-b-2 border-gray-200 px-6 lg:px-8">
         <div className="flex h-16 items-center justify-between">
           <div className="flex items-center">
             <Link href="/" className="flex-shrink-0">
