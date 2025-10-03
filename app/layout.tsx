ee879d44e3149df2253c267ce6c5bb600dd37af4
Vibo <vahab.afsharian94@gmail.com>
2025-10-03 10:36:29 +0300

    fix: add suppressHydrationWarning to html tag for improved hydration handling

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,7 +27,7 @@ export default async function RootLayout({
 }: Readonly<{ children: React.ReactNode }>) {
   return (
     <ClerkProvider>
-      <html lang="en">
+      <html lang="en" suppressHydrationWarning>
         <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         >
